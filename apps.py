57d3ae81742f7e727ce5ef435acd935c70412e53
黄哲骁 <hzhuangzhexiao@corp.netease.com>
2016-09-01 11:07:16 +0800

    add apps.py

--- apps.py
+++ apps.py
@@ -1,1 +1,32 @@
+import requests
+import json
 from utils import *
+from json_tabulate import *
+
+def get_app_images(token):
+    headers = {'Content-type': 'application/json',
+               'Authorization': 'Token {}'.format(token)}
+    r = requests.get(ENV + '/api/v1/apps/images', headers=headers)
+    result_json = json.loads(r.text)
+    return result_json
+
+def do_app_image_list(app_key, app_secret):
+    token = get_token(app_key, app_secret)
+    image_result_json = get_app_images(token)
+
+    headers = ["id", "name", "tag"]
+    json_key_list = ["custom_images", "public_images"]
+    headers, tabulate_data_list = json_tabulate(image_result_json, json_key_list, headers)
+    tabulate_print_info(headers, tabulate_data_list)
+
+
+
+def tabulate_print_info(headers, result_metadata_list):
+    print tabulate(result_metadata_list, headers, tablefmt="psql", stralign="left", numalign="text")
+
+
+if __name__ == '__main__':
+    do_app_image_list(ACCESS_KEY, ACCESS_SECRET)
+
+
+
