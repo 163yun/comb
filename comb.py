641ac5d584be28cdf0dd2fc21730fe0325576b48
黄哲骁 <hzhuangzhexiao@corp.netease.com>
2016-09-01 10:18:25 +0800

    add single_json_tabulate

--- comb.py
+++ comb.py
@@ -34,6 +34,17 @@ def json_tabulate(json, json_key_list, headers_list):
     return headers, tabulate_data_list
 
 
+def single_json_tabulate(json, json_key_list):
+    tabulate_data_list = []
+
+    for key in json_key_list:
+        result_list = {key: json[key]}
+        tabulate_data_list.append([key, result_list[key]])
+
+    headers = ["Field", "Value"]
+    return headers, tabulate_data_list
+
+
 def get_token(app_key, app_secret):
     headers = {'Content-type': 'application/json'}
     post_data = {
@@ -48,10 +59,18 @@ def get_token(app_key, app_secret):
     return token
 
 
-def get_images(token):
+def get_container_images(token):
     headers = {'Content-type': 'application/json',
                'Authorization': 'Token {}'.format(token)}
     r = requests.get(ENV + '/api/v1/containers/images', headers=headers)
+    result_json = json.loads(r.text)
+    return result_json
+
+
+def get_container_info(token, containerId):
+    headers = {'Content-type': 'application/json',
+               'Authorization': 'Token {}'.format(token)}
+    r = requests.get(ENV + '/api/v1/containers/{containerId}'.format(containerId=containerId), headers=headers)
     result_json = json.loads(r.text)
     return result_json
 
@@ -64,17 +83,35 @@ def get_containers(token):
     return result_json
 
 
+def get_repositories(token):
+    headers = {'Content-type': 'application/json',
+               'Authorization': 'Token {}'.format(token)}
+    r = requests.get(ENV + '/api/v1/repositories', headers=headers)
+    result_json = json.loads(r.text)
+    return result_json
+
+
 def tabulate_print_info(headers, result_metadata_list):
     print tabulate(result_metadata_list, headers, tablefmt="psql", stralign="left", numalign="text")
 
 
-def do_image_list(app_key, app_secret):
+def do_container_image_list(app_key, app_secret):
     token = get_token(app_key, app_secret)
-    image_result_json = get_images(token)
+    image_result_json = get_container_images(token)
 
     headers = ["id", "name", "tag"]
     json_key_list = ["custom_images", "public_images"]
     headers, tabulate_data_list = json_tabulate(image_result_json, json_key_list, headers)
+    tabulate_print_info(headers, tabulate_data_list)
+
+
+def do_container_show(app_key, app_secret, containerId):
+    token = get_token(app_key, app_secret)
+    container_result_json = get_container_info(token, containerId)
+
+    headers = ["id", "name", "tag"]
+    json_key_list = ["id", "name", "status","bandwidth","public_ip","image_id"]
+    headers, tabulate_data_list = single_json_tabulate(container_result_json, json_key_list)
     tabulate_print_info(headers, tabulate_data_list)
 
 
@@ -88,6 +125,17 @@ def do_container_list(app_key, app_secret):
     tabulate_print_info(headers, tabulate_data_list)
 
 
+def do_repositories_list(app_key, app_secret):
+    token = get_token(app_key, app_secret)
+    repositories_result_json = get_repositories(token)
+
+    headers = ["repo_id", "repo_name", "created_at"]
+    json_key_list = ["repositories"]
+    headers, tabulate_data_list = json_tabulate(repositories_result_json, json_key_list, headers)
+    tabulate_print_info(headers, tabulate_data_list)
+
+
 if __name__ == '__main__':
-    do_image_list(ACCESS_KEY, ACCESS_SECRET)
-    do_container_list(ACCESS_KEY, ACCESS_SECRET)
+    # do_container_list(ACCESS_KEY, ACCESS_SECRET)
+    # do_repositories_list(ACCESS_KEY, ACCESS_SECRET)
+    do_container_show(ACCESS_KEY,ACCESS_SECRET,"347339")
