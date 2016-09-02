01d6e5783d40bf7eeea3eaaf6c5cfee1fcaadd41
黄哲骁 <hzhuangzhexiao@corp.netease.com>
2016-09-02 10:20:26 +0800

    add comment for json tabulate

--- json_tabulate.py
+++ json_tabulate.py
@@ -9,7 +9,17 @@ import click
 
 
 def multi_column_json_tabulate(json, json_key_list, headers_list):
-    # second_key_list is headers
+    # handle column like this
+    # +--------+------------+-------------+---------------+------------+
+    # |     id | name       | status      | public_ip     |   image_id |
+    # |--------+------------+-------------+---------------+------------|
+    # | 630831 | testNagios | create_succ | 59.111.91.56  | 26413      |
+    # | 628306 | test       | create_succ | 59.111.91.23  | 21697      |
+    # | 193887 | myss2      | create_succ | 59.111.72.128 | 30656      |
+    # +--------+------------+-------------+---------------+------------+
+
+
+    # second_key_list define headers (e.g. ["id", "name", "status", "public_ip", "image_id"])
     # first key list is the list prepared to parse
     tabulate_data_list = []
 
@@ -31,6 +41,29 @@ def multi_column_json_tabulate(json, json_key_list, headers_list):
 
 
 def two_columns_json_tabulate(json, json_key_list):
+    # handle column like this
+    # +---------------------+----------------------+
+    # | Field               | Value                |
+    # |---------------------+----------------------|
+    # | id                  | 628306               |
+    # | bandwidth           | 100                  |
+    # | charge_type         | 1                    |
+    # | created_at          | 2016-09-01T09:27:07Z |
+    # | desc                |                      |
+    # | env_var             | {}                   |
+    # | image_id            | 21697                |
+    # | name                | test                 |
+    # | network_charge_type | 2                    |
+    # | private_ip          | 10.173.32.61         |
+    # | public_ip           | 59.111.91.23         |
+    # | replicas            | 1                    |
+    # | spec_id             | 1                    |
+    # | ssh_key_ids         |                      |
+    # | status              | create_succ          |
+    # | updated_at          | 2016-09-01T09:27:40Z |
+    # | use_public_network  | 1                    |
+    # +---------------------+----------------------+
+
     tabulate_data_list = []
 
     for key in json_key_list:
